5a7b45f6830dc43e289f944cb0fc3d74e9819acb
Aniket Parashar <aniket.parashar04@gmail.com>
2024-02-25 19:03:02 +0530

    first commit

---
+++ middleware.ts
@@ -0,0 +1,20 @@
+// import { withClerkMiddleware } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
+
+// export default withClerkMiddleware((req: NextRequest) => {
+//   return NextResponse.next();
+// });
+
+// // Stop Middleware running on static files
+// export const config = {
+//   matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+// };
+
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
